d81c51b5c61ef541a5a998e4190c47b782593433
LeonLow97 <lowjiewei@gmail.com>
2023-08-19 16:37:25 +0800

    Added helper function in golang api

--- trevor_sawler/vue-api/cmd/api/handlers.go
+++ trevor_sawler/vue-api/cmd/api/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -19,22 +18,12 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 	var creds credentials
 	var payload jsonResponse
 
-	// decode the request body into type credentials
-	err := json.NewDecoder(r.Body).Decode(&creds)
+	err := app.readJSON(w, r, creds)
 	if err != nil {
-		// send back error message
-		app.errorLog.Println("invalid json")
+		app.errorLog.Println(err)
 		payload.Error = true
-		payload.Message = "Invalid JSON"
-
-		out, err := json.MarshalIndent(payload, "", "\t")
-		if err != nil {
-			app.errorLog.Println(err)
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write(out)
+		payload.Message = "invalid json supplied, or json missing entirely"
+		_ = app.writeJSON(w, http.StatusBadRequest, payload)
 		return
 	}
 
@@ -44,12 +33,10 @@ func (app *application) Login(w http.ResponseWriter, r *http.Request) {
 	// send back a response
 	payload.Error = false
 	payload.Message = "Signed In"
-	out, err := json.MarshalIndent(payload, "", "\t")
+
+	// out, err := json.MarshalIndent(payload, "", "\t")
+	err = app.writeJSON(w, http.StatusOK, payload)
 	if err != nil {
 		app.errorLog.Println(err)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
 }
